17ca6b25ab0e09580f2635b56bfc8bf70a2f2f90
thBoudge <thomas.bouges@gmail.com>
2018-11-14 11:09:03 -0500

    modification of func imagePickerController pour ne pas multiplier les tapGwestureRecognizer sur UiImage, utilisation de Notification.default.observer ala place de viewWillTransfer afin de corriger le bug de chargement application ou le swipe landscape ne fonctionnait pas

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -35,30 +35,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         //initiate gridView form
         gridView.grid = .twoOne
-        
         imagePickerController.delegate = self
-        
-        
         //swipe left or up
-        // URL : https://www.youtube.com/watch?v=BZ_Ke0dYpdw
-        
         swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleShareAction))
-       setUpSwipeDirection()
         guard let swipeGesture = swipeGesture else {return}
-        
         gridView.addGestureRecognizer(swipeGesture)
-       
-      ///////
-        if (topLeftImageView == nil || botLeftButtonAddImage == nil ||
-            gridView.grid == .oneTwo && botRightButtonAddImage == nil || gridView.grid == .twoOne && topRightImageView == nil) {}
-        
-        
+       // observer for orientation
+        NotificationCenter.default.addObserver(self, selector: #selector(setUpSwipeDirection), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
+    
     //Swipe func to share
     @objc func handleShareAction (){
-        //let screenWidth = UIScreen.main.bounds.width
-       // let screenHeight = UIScreen.main.bounds.height
-        
         var translationTransform: CGAffineTransform
         if swipeGesture?.direction == .up {
             print("swipe up")
@@ -72,7 +59,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                     self.gridView.transform = translationTransform
                     }, completion: { (success) in
                         if success {
-                            ///////ne marche pas///////
+                            //Bloc swipe if not all Uiimageview have been load with an image
                             if (self.topLeftImageView.image == nil || self.botLeftImageView.image == nil ||
                                 ((self.gridView.grid == .oneTwo || self.gridView.grid == .four) && self.botRightImageView.image == nil) || (self.gridView.grid == .twoOne || self.gridView.grid == .four) && self.topRightImageView.image == nil) {
                                 
@@ -84,41 +71,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                             else {self.sharingGrid()}
                         }
                     })
-        
     }
     
     // function in order to share gridView
     // URL : https://www.youtube.com/watch?v=6o4PmMywIA8
     func sharingGrid(){
-        /*
-        //gridView appear in this place with animation
-        gridView.transform = .identity
-        // We reduce size of grid view in order to create after thet a zoom effect
-        gridView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-        //We animate grid view in order it get back to this previous form
-        UIView.animate(withDuration: 0.4,  animations: {
-        self.gridView.transform = .identity
-        }, completion:nil)
-        */
-        
         guard let imageGrid = gridView.convertToImage() else {return}
-        
         let activityViewController = UIActivityViewController(activityItems: [imageGrid], applicationActivities: nil)
         
         present(activityViewController, animated: true, completion: nil)
-        
         // gridView appear after activity activityviewcontroller ended
         activityViewController.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
             // Return if cancelled
             if (!completed) {
                 self.gridViewReturn()
             }
-
         }
-        
-        
-    }
+     }
     
+    // function for animate gridView return after a swipe
     func gridViewReturn () {
         //gridView appear in this place with animation
         self.gridView.transform = .identity
@@ -128,11 +99,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         UIView.animate(withDuration: 0.4,  animations: {
             self.gridView.transform = .identity
         }, completion:nil)
-        
     }
     
-    //func portraut vs landscape
-    func setUpSwipeDirection (){
+    //func portrait vs landscape
+    @objc func setUpSwipeDirection (){
         if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight { //UIDevice.current.orientation.isLandscape
             print("Landscape")
             swipeGesture?.direction = .left } else {
@@ -144,15 +114,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     // function to change button and grid
     @IBAction func gridButton(_ sender: UIButton) {
-        
         // for each to change button form
         gridPatternButtons.forEach { $0.isSelected = false }
         sender.isSelected = true
-        
         // symbolise 1 element de t bouton
         gridPatternButtons.forEach { $0.isSelected = false }
         sender.isSelected = true
-        
         switch sender.tag {
         case 0:
             gridView.grid = .oneTwo
@@ -193,55 +160,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                                                     self.imagePickerController.sourceType = .camera
                                                     self.present(self.imagePickerController, animated: true, completion: nil)
                                                 }else{
-                                                    let alertCamera = UIAlertController(title: "error Missing Image", message: "Missing picture please add one", preferredStyle: UIAlertController.Style.alert)
+                                                    //error alert message
+                                                    let alertCamera = UIAlertController(title: "error No Camera available", message: "Camera not found", preferredStyle: UIAlertController.Style.alert)
                                                     alertCamera.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
                                                     self.present(alertCamera, animated: true, completion: nil)
-                                                    
                                                 }
-                                               
         }))
-        
         actionSheet.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: nil))
-    
         self.present(actionSheet,animated: true,completion: nil)
-        
     }
     
-    // function pour aller chercher image dans la library
+    // function to add picture from Library
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        
         // a revoir
         guard let tag = imageInt else { return }
         let imageViews = [topLeftImageView,topRightImageView,botLeftImageView,botRightImageView]
         let buttons = [topLeftButtonAddImage,topRightButtonAddImage,botLeftButtonAddImage,botRightButtonAddImage]
-        
         let imageView = imageViews[tag]
         let button = buttons[tag]
-        //add image
-        imageView?.image = image
         
-        // Button +
-        button?.isHidden = true
-        
-        // initialisation of tapGesture on ImageView
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapImageView(recogniser:)))
-        imageView?.addGestureRecognizer(tapGestureRecognizer)
-        //////// gesture sur cette photo voir si on en créé une autre ou pas si oui evité car ce repete si on clique sur photo
-
-        picker.dismiss(animated: true, completion: nil)
+        //If in order to do not have multiple tapGesture on image
+        if button?.isHidden == true {
+            imageView?.image = image
+            picker.dismiss(animated: true, completion: nil)
+        }else {
+            //add image
+            imageView?.image = image
+            // Button +
+            button?.isHidden = true
+            // initialisation of tapGesture on ImageView
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapImageView(recogniser:)))
+            imageView?.addGestureRecognizer(tapGestureRecognizer)
+            picker.dismiss(animated: true, completion: nil)
+        }
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
-    
-    // a voir changer par notify pblm setup swipe
+    /*Correct mais le view willtransfert ne prends pas en compte le mode landscape en mode landscape , mieux vaux utiliser notice Observer
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         setUpSwipeDirection()
+    }*/
     }
-    
-  
-}
 
